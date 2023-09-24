094addffcf6917cdc2f5fe98179e14ffb9070572
Prakhar Sinha <prakhar.sinha2k2@gmail.com>
2023-09-25 01:01:05 +0530

    starting login api, password encryption using bcryptjs

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,11 @@ const cors=require('cors')
 const exp = require('constants')
 const { default: mongoose } = require('mongoose')
 const User=require('./models/User')
+const bcrypt=require('bcryptjs')
 const app=express()
 
+
+const salt=bcrypt.genSaltSync()
 app.use(cors())
 app.use(express.json())
 
@@ -17,13 +20,35 @@ app.post('/register',async (req,res)=>{
         const userInfo=await User.create({
             name:name,
             email:email,
-            password:password 
+            password: bcrypt.hashSync(password,salt)
         })
         res.status(200).json({'status':"User Registered Successfully!"})
     }
     catch(e){
         res.status(400).json({'status':"Something Went Wrong!"})
     }
+})
+
+app.post('/login',async(req,res)=>{
+    const {email,password}=req.body;
+
+    const userInfo=await User.findOne({email})
+    if(!userInfo.email){
+        res.status(400).json({'err':"User Doesn't Exist!"})
+    }
+
+    const passCheck=bcrypt.compareSync(password,userInfo.password)          //.compareSync functn of bcypt will return false if pwd doesn't match
+    if(!passCheck){
+        res.status(400).json({'err':"Invalid Password: Please Try Again!"})
+    }
+
+    else{                              //means password is correct, that measn this is the stage, that user has logged in!
+
+    }
+
+
+    
+
 })
 
 app.listen(4000,()=>{console.log("Server Running On PORT NO: 4000")})
--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,16 +1,23 @@
-import { useState } from "react"
+import { useState,useEffect } from "react"
 
 export default function LoginPage(){
 
-    const [name,setName]=useState("")
     const [email,setEmail]=useState("")
     const [password,setPassword]=useState("")
 
+    const handleLogin=async (e)=>{
+        e.preventDefault()
+
+        const data=fetch('http://localhost:4000/login',{
+            method:'POST',
+            body:JSON.stringify({email,password}),
+            headers:{'Content-Type':'application/json'}
+        })
+    }
 
     return(
-       <form className="login" >
+       <form className="login" onSubmit={handleLogin}>
         <h2>Login</h2>
-        <input type="text" placeholder="name" value={name} onChange={(e)=>(setName(e.target.value))} />
         <input type="text" placeholder="email" value={email} onChange={(e)=>(setEmail(e.target.value))} />
         <input type="password" placeholder="password" value={password} onChange={(e)=>(setPassword(e.target.value))}/>
         <button>Login</button>
